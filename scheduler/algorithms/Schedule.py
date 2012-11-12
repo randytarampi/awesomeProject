fbaca3d339e4357059282a0ab735f1aa8327025a
clocke <conradlocke0@gmail.com>
2012-11-11 18:49:25 -0800

    First iteration of the scheduler algorithm (we have the ability to add, free, and lock courses)    we can also build a schedule from the ground up with certain courses

--- scheduler/algorithms/Schedule.py
+++ scheduler/algorithms/Schedule.py
@@ -1,5 +1,7 @@
 #Schedule.py
 
+
+
 class Schedule:
     mondayTimeSlotAvailability = [0] * 144
     tuesdayTimeSlotAvailability = [0] * 144
@@ -10,15 +12,66 @@ class Schedule:
     sundayTimeSlotAvailability = [0] * 144
     #def getTotalDays(self):
 
+    #see if the timeslice conflicts with the current weekly schedule
+    def checkTimeWeekConflict(self, startTime, endTime, weekday):
+        if weekday ==0:
+            return checkIfTimeDayConflict(startTime, endTime, self.mondayTimeSlotAvailability)
+        elif weekday ==1:
+            return checkIfTimeDayConflict(startTime, endTime, self.tuesdayTimeSlotAvailability)
+        elif weekday ==2:
+            return checkIfTimeDayConflict(startTime, endTime, self.wednesdayTimeSlotAvailability)
+    
+        elif weekday ==3:
+            return checkIfTimeDayConflict(startTime, endTime, self.thursdayTimeSlotAvailability)
+
+        elif weekday ==4:
+            return checkIfTimeDayConflict(startTime, endTime, self.fridayTimeSlotAvailability)
+
+        elif weekday ==5:
+            return checkIfTimeDayConflict(startTime, endTime, self.saturdayTimeSlotAvailability)
+
+        elif weekday ==6:
+            return checkIfTimeDayConflict(startTime, endTime, self.sundayTimeSlotAvailability)
+
+    #i.e. input 0 to get monday, 1 to get tuesday... 6 to get sunday
+    def convertWeekDayToProperArray(self, weekday):
+        if weekday ==0:
+            return self.mondayTimeSlotAvailability
+        elif weekday ==1:
+            return self.tuesdayTimeSlotAvailability
+        elif weekday ==2:
+            return self.wednesdayTimeSlotAvailability
+        elif weekday ==3:
+            return self.thursdayTimeSlotAvailability
+        elif weekday ==4:
+            return self.fridayTimeSlotAvailability
+        elif weekday ==5:
+           return self.saturdayTimeSlotAvailability
+        elif weekday ==6:
+           return self.sundayTimeSlotAvailability
+
+    #Locks slots start-->end, on the given workday
+    def lockMeetingTime(self, startTime, endTime, weekday):
+        lockSlotThrough(startTime, endTime, self.convertWeekDayToProperArray(weekday))
+    #Frees slots start-->end, on the given workday
+    def unlockMeetingTime(self, startTime, endTime, weekday):
+        unlockSlotThrough(startTime, endTime, self.convertWeekDayToProperArray(weekday))
+    #Sets slots start-->end, on the given workday
+    def setMeetingTime(self, startTime, endTime, weekday):
+        setSlotThrough(startTime, endTime, self.convertWeekDayToProperArray(weekday))
+    def freeMeetingTime(self, startTime, endTime, weekday):
+        freeSlotThrough(startTime, endTime, self.convertWeekDayToProperArray(weekday))
+
+    
     def getTotalTimeGap(self):
         weekTotalTimeGap = 0
-        weekTotalTimeGap += getTimeGapForSchedule(self.mondayTimeSlotAvailability)
-        weekTotalTimeGap += getTimeGapForSchedule(self.tuesdayTimeSlotAvailability)
-        weekTotalTimeGap += getTimeGapForSchedule(self.thursdayTimeSlotAvailability)
-        weekTotalTimeGap += getTimeGapForSchedule(self.wednesdayTimeSlotAvailability)
-        weekTotalTimeGap += getTimeGapForSchedule(self.fridayTimeSlotAvailability)
-        weekTotalTimeGap += getTimeGapForSchedule(self.saturdayTimeSlotAvailability)
-        weekTotalTimeGap += getTimeGapForSchedule(self.sundayTimeSlotAvailability)
+        weekTotalTimeGap += getTimeGapForDay(self.mondayTimeSlotAvailability)
+        weekTotalTimeGap += getTimeGapForDay(self.tuesdayTimeSlotAvailability)
+        weekTotalTimeGap += getTimeGapForDay(self.thursdayTimeSlotAvailability)
+        weekTotalTimeGap += getTimeGapForDay(self.wednesdayTimeSlotAvailability)
+        weekTotalTimeGap += getTimeGapForDay(self.fridayTimeSlotAvailability)
+        weekTotalTimeGap += getTimeGapForDay(self.saturdayTimeSlotAvailability)
+        weekTotalTimeGap += getTimeGapForDay(self.sundayTimeSlotAvailability)
         return weekTotalTimeGap
     def getTotalDays(self):
         totalDays = 0
@@ -48,47 +101,66 @@ class Schedule:
         self.saturdayTimeSlotAvailability = [0] * 144
         self.sundayTimeSlotAvailability = [0] * 144
     def clearSchedule(self):
-        for i in range (0, len(self.mondayTimeSlotAvailability):
+        for i in range (0, len(self.mondayTimeSlotAvailability)):
             if self.mondayTimeSlotAvailability[i] == 1:
                 self.mondayTimeSlotAvailability[i] = 0 
-        for i in range (0, len(self.tuesdayTimeSlotAvailability):
+        for i in range (0, len(self.tuesdayTimeSlotAvailability)):
             if self.tuesdayTimeSlotAvailability[i] == 1:
                 self.tuesdayTimeSlotAvailability[i] = 0
-        for i in range (0, len(self.wednesdayTimeSlotAvailability):
+        for i in range (0, len(self.wednesdayTimeSlotAvailability)):
             if self.wednesdayTimeSlotAvailability[i] == 1:
                 self.wednesdayTimeSlotAvailability[i] = 0
-        for i in range (0, len(self.thursdayTimeSlotAvailability):
+        for i in range (0, len(self.thursdayTimeSlotAvailability)):
             if self.thursdayTimeSlotAvailability[i] == 1:
                 self.thursdayTimeSlotAvailability[i] = 0
-        for i in range (0, len(self.fridayTimeSlotAvailability):
+        for i in range (0, len(self.fridayTimeSlotAvailability)):
             if self.fridayTimeSlotAvailability[i] == 1:
                 self.fridayTimeSlotAvailability[i] = 0
-        for i in range (0, len(self.saturdayTimeSlotAvailability):
+        for i in range (0, len(self.saturdayTimeSlotAvailability)):
             if self.saturdayTimeSlotAvailability[i] == 1:
                 self.saturdayTimeSlotAvailability[i] = 0
-        for i in range (0, len(self.sundayTimeSlotAvailability):
+        for i in range (0, len(self.sundayTimeSlotAvailability)):
             if self.sundayTimeSlotAvailability[i] == 1:
                 self.sundayTimeSlotAvailability[i] = 0
                 
     #totalPurge and clear schedule should be methods here
 
+def checkIfTimeDayConflict(startTime, endTime, timeSlotArray):
+    for i in range (startTime, endTime+1):
+        if timeSlotArray[i] != 0:
+            return True
+    return False
+
+#def checkIfTimeSpaceOpen(startTime, endTime, timeSlotArray):
+##     for i in range (startTime, endTime):
+ #       if timeSlotArray[i] != 0:
+ #           return true
+ #   return f
+
+
 def lockSlotThrough(startTime, endTime, timeSlotArray):
-    for x in range (startTime, endTime):
+    for x in range (startTime, endTime+1):
         timeSlotArray[x] = 2
 
 def setSlotThrough(startTime, endTime, timeSlotArray):
-    for x in range (startTime, endTime):
+    for x in range (startTime, endTime+1):
         timeSlotArray[x] = 1
 
-def freeSlotThrough(startTime, endTime, timeSlotArray):
-    for x in range (startTime, endTime):
+def unlockSlotThrough(startTime, endTime, timeSlotArray):
+    for x in range (startTime, endTime+1):
         timeSlotArray[x] = 0
+        
+def freeSlotThrough(startTime, endTime, timeSlotArray):
+    for x in range (startTime, endTime+1):
+        if timeSlotArray[x] != 2:
+           timeSlotArray[x] = 0
+        
 
 def checkSlot(slotNumber, timeSlotArray):
     return timeSlotArray[slotNumber]
 
 #Finds the total amout of gap in the schedule for one timeSlotArray
-def getTimeGapForSchedule(timeSlotArray):
+def getTimeGapForDay(timeSlotArray):
     totalGap = 0
     marker = -1
     for i in range (0, len(timeSlotArray)):
@@ -101,12 +173,4 @@ def getTimeGapForSchedule(timeSlotArray):
                 marker = i
     return totalGap
         
-#def freeSlotThrough(startTime, endTime, timeSlotArray):
-#    for x in range (startTime, endTime):
-#        timeSlotArray[x] = 0
-
-#def 
-
-# get total gap
-
 
