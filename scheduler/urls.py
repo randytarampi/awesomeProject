52e78c71eb6fe9c0d3568de056fb5c2b4fe6cd85
Randy Tarampi <randy.tarampi@gmail.com>
2012-11-13 11:44:07 -0800

    Randy pushing everything before Steven starts working...

--- scheduler/urls.py
+++ scheduler/urls.py
@@ -2,4 +2,6 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('scheduler.views',
     url(r'^$', 'index', name = "scheduler_index"),
+    url(r'^instructions/', 'instructions', name = "scheduler_instructions"),
+    url(r'^examples/', 'examples', name = "scheduler_examples"),
 )
