4231607228b74f0e77b8311a4a5ba373259177c3
Steven Evans <stevene@sfu.ca>
2012-11-13 15:19:34 -0800

    Modified ajax.py to have separate drop downs per course. Removed example tutorial code from schedulerIndex.html.

--- scheduler/ajax.py
+++ scheduler/ajax.py
@@ -1,26 +1,22 @@
 from dajaxice.decorators import dajaxice_register
 from dajax.core import Dajax
+from scheduler.models import *
+
+def listOfSubjects():
+	allSubjects = Course.objects.values_list('subject', flat=True).distinct()
+	datList = []
+	for i in allSubjects:
+		datList.append("<option value='%s'>%s</option>" % (i, i))
+	return ''.join(datList)
 
 @dajaxice_register
 def updatingCourseForm(request, option):
 	dajax = Dajax()
 
 	out = []
-	for aClass in range(0, int(option)):
-		out.append("<p>Course %s</p>" % str(aClass))
+	for aClass in range(1, int(option)+1):
+		out.append('<div>Course %s: &nbsp<select id="courseNumber%s">%s</select></div>' % (str(aClass), str(aClass), listOfSubjects()))
+		#dajax.assign('#courseNumber%s' % str(aClass), 'innerHTML', listOfSubjects())
 
 	dajax.assign('#listClasses', 'innerHTML', ''.join(out))
 	return dajax.json()
-
-@dajaxice_register
-def updatecombo(request, option):
-    dajax = Dajax()
-    options = [['Madrid', 'Barcelona', 'Vitoria', 'Burgos'],
-               ['Paris', 'Evreux', 'Le Havre', 'Reims'],
-               ['London', 'Birmingham', 'Bristol', 'Cardiff']]
-    out = []
-    for option in options[int(option)]:
-        out.append("<option value='#'>%s</option>" % option)
-
-    dajax.assign('#combo2', 'innerHTML', ''.join(out))
-    return dajax.json()
