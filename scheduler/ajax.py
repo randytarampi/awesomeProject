31b3609ef8643a5ae244da5e5baf7f26f52a5ead
Steven Evans <stevene@sfu.ca>
2012-11-13 17:41:50 -0800

    ajax.py currently not working as expected but currently a working process. The last few commits brought to you in part by: An Afternoon With Randy.

--- scheduler/ajax.py
+++ scheduler/ajax.py
@@ -5,18 +5,29 @@ from scheduler.models import *
 def listOfSubjects():
 	allSubjects = Course.objects.values_list('subject', flat=True).distinct()
 	datList = []
+	# runs through every subject and makes an option for a select tag where value = Subject
 	for i in allSubjects:
 		datList.append("<option value='%s'>%s</option>" % (i, i))
 	return ''.join(datList)
+
+def listOfNumbers(request, option):
+	daList = Course.objects.filter(subject=option).values_list('number', flat=True)
+	htmlList = []
+	j = 0
+	for i in daList:
+		htmlList.append("<option value='%s'>%s</option>" % (i, i))
+		dajax.assign('#courseNumber%d' % str(j), 'innerHTML', ''.join(htmlList))
+		j += 1;
 
 @dajaxice_register
 def updatingCourseForm(request, option):
 	dajax = Dajax()
 
 	out = []
+	# several select tags are made, each with a complete list of subjects with value = 1 through aClass
 	for aClass in range(1, int(option)+1):
-		out.append('<div>Course %s: &nbsp<select id="courseNumber%s">%s</select></div>' % (str(aClass), str(aClass), listOfSubjects()))
-		#dajax.assign('#courseNumber%s' % str(aClass), 'innerHTML', listOfSubjects())
+		out.append('<div>Course %s: <select id="courseSubject%s" onchange=%s>%s</select> &nbsp<select id="courseNumber%s"></select></div>' % (str(aClass), str(aClass), "Dajaxice.scheduler.listOfNumbers(Dajax.process, {'option':this.value})", listOfSubjects(), str(aClass)))
 
 	dajax.assign('#listClasses', 'innerHTML', ''.join(out))
 	return dajax.json()
+
--- settings.py
+++ settings.py
@@ -111,7 +111,7 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.media',
     'django.core.context_processors.static',
     'django.core.context_processors.request',
-    'django.contrib.messages.context_processors.messages'
+    'django.contrib.messages.context_processors.messages',
 )
 
 MIDDLEWARE_CLASSES = (
