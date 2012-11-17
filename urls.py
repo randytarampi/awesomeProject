36827f810eb0ff60350c34c71508d794d6f9f5cb
Randy Tarampi <randy.tarampi@gmail.com>
2012-11-16 23:53:17 -0800

    Changes to robots.txt and enabling of the favicon

--- urls.py
+++ urls.py
@@ -1,7 +1,7 @@
 from django.conf import settings
 from django.conf.urls.defaults import *
 from django.conf.urls.static import static
-from django.views.generic.simple import direct_to_template
+from django.views.generic.simple import direct_to_template, redirect_to
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 from dajaxice.core import dajaxice_autodiscover, dajaxice_config
 
@@ -26,6 +26,10 @@ urlpatterns = patterns('',
     # Dajax/Dajaxice URLs
     url(dajaxice_config.dajaxice_url, include('dajaxice.urls')),
 
+    # Favicon & robots.txt
+    url(r'^robots\.txt$', direct_to_template, {'template': 'robots.txt', 'mimetype': 'text/plain'}),
+    url(r'^favicon\.ico$', redirect_to, {'url': '/static/favicon.ico'}),
+
     # Project URLs
     url(r'^$', direct_to_template, {'template': 'index.html'}, name="index"),
     url(r'^about/', include('about.urls')),
@@ -33,4 +37,3 @@ urlpatterns = patterns('',
 )
 
 urlpatterns += staticfiles_urlpatterns()
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
