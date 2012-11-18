9dfab50cead6769144cd6d6e273c394f990937a6
clocke <conradlocke0@gmail.com>
2012-11-17 20:04:05 -0800

    Fixed bugs in algorithm (funcion for randy) code is still messy... will cleanup

---
+++ scheduler/choicestats.py
@@ -0,0 +1,7 @@
+#choicestats.py
+
+class ChoiceStats:
+    def __init__(self, changeNumberOfDays, changeTotalGap):
+        self.changeNumberOfDays = changeNumberOfDays
+        self.changeTotalGap = changeTotalGap
+
