7232cdc3fb4829c20f43621dfc8e4400a0478d29
clocke <conradlocke0@gmail.com>
2012-11-28 16:06:37 -0800

    Added new system for handling labs. filtered out distance courses and altered the indenting for algorithm.py

--- scheduler/course.py
+++ scheduler/course.py
@@ -1,15 +1,16 @@
 #course.py
 
 class SchedulingCourse:
-	def __init__(self, title, courseID, meetingTimes, exams, campus):
+	def __init__(self, title, courseID, meetingTimes, exams, labs, campus):
 		self.title = title
 		self.courseID = courseID
 		self.meetingTimes = meetingTimes# list of meeting times
 		self.exams = exams
+		self.labs = labs
 		self.campus = campus # 1 = Burnaby, 2 = surrey, 3 = vancouver, 0 = something else
 		
         #we may want to include the id in course here...
-    	def addMeetingTimes(self, meetingTime):
+    	def addMeetingTime(self, meetingTime):
         	self.meetingTimes.append(meetingTime)
     		#priority ... number
    	 	#has priority? boolean
