81f017694ea98ffe6c66aa38cb2783a06c0c367b
Steven Evans <stevene@sfu.ca>
2012-11-30 23:10:50 -0800

    Subject list now appears within view. Will do further changes.

--- scheduler/views.py
+++ scheduler/views.py
@@ -2,9 +2,12 @@ from django.shortcuts import get_object_or_404, render_to_response
 from django.http import HttpResponseRedirect, HttpResponse
 from django.core.urlresolvers import reverse
 from django.template import RequestContext
+from scheduler.models import *
 
 def index(request):
-    return render_to_response('schedulerIndex.html', context_instance=RequestContext(request))
+    allSubjects = Course.objects.values_list('subject', flat=True).distinct()
+    print allSubjects
+    return render_to_response('schedulerIndex.html',{'subjects': allSubjects}, context_instance=RequestContext(request))
 
 def instructions(request):
     return render_to_response('schedulerInstructions.html')
