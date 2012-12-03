8dd317fcd05e6a499b44a0c56b122ce4569f897c
Steven Evans <stevene@sfu.ca>
2012-12-02 19:23:02 -0800

    apparently we didnt actually need the dajax.clear call. Changed the list of numbers from profs option values to the primary key value and made the initial values for the add course by instructor form. Hoorah

--- scheduler/views.py
+++ scheduler/views.py
@@ -6,9 +6,22 @@ from scheduler.models import *
 
 def index(request):
 	allSubjects = Course.objects.values_list('subject', flat=True).distinct()
+
+	# Using the 0-th element so that any set of data will give the first element. This is using the same query as in ajax.py
 	firstSubject = allSubjects[0]
 	initialNumbers = Course.objects.filter(subject=firstSubject).values_list('number', flat=True).distinct()
-	return render_to_response('schedulerIndex.html', { 'subjects': allSubjects, 'initNums': initialNumbers }, context_instance=RequestContext(request))
+
+	# More initial values, w.r.t. instructor form this time 	
+	c = Course.objects.filter(subject=firstSubject)
+	initialProfs = Instructor.objects.filter(course__in=c).order_by('last_name').distinct()
+
+	instructorsCourses = []
+	d = Instructor.objects.filter(userid=initialProfs[0])
+	for i in d:
+		for j in i.course.all():
+			instructorsCourses.append(j)
+
+	return render_to_response('schedulerIndex.html', { 'subjects': allSubjects, 'initNums': initialNumbers, 'initProfs': initialProfs, 'initNumsByProf': instructorsCourses }, context_instance=RequestContext(request))
 
 def instructions(request):
 	return render_to_response('schedulerInstructions.html')
