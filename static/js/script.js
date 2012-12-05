8c2a6cd36ff02d91086ffe531462c729f8184e3e
Randy Tarampi <randy.tarampi@gmail.com>
2012-12-04 20:53:54 -0800

    A fix for the wonky JS issues regarding collapsible lists. Some cleanup of the course detail view.

--- static/js/script.js
+++ static/js/script.js
@@ -29,7 +29,35 @@ function jQueryEffects() {
 }
 
 $(document).ready(
-	jQueryEffects()
+	function () {
+		// Rejected Courses & Times
+		$("li.rejected").toggle(
+			function() {
+				$(this).next().show('fast');
+				$(this).removeClass('toggleExpand');
+				$(this).addClass('toggleCollapse');
+			},
+			function() {
+				$(this).next().hide('slow');
+				$(this).removeClass('toggleCollapse');
+				$(this).addClass('toggleExpand');
+			}
+	 	);
+	 	
+	 	// Optimal Courses
+		$("li.optimal").toggle(
+			function() {
+				$(this).next().hide('slow');
+				$(this).removeClass('toggleCollapse');
+				$(this).addClass('toggleExpand');
+			},
+			function() {
+				$(this).next().show('fast');
+				$(this).removeClass('toggleExpand');
+				$(this).addClass('toggleCollapse');
+			}
+	 	);
+ 	}
 );
 
 function generateSchedule() {
